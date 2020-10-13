57415039ac698f636fa4af96a25c96a2be8945de
mjoppich <github@mjoppich.net>
2020-10-13 15:28:07 +0200

    readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+sys.path.insert(0, os.path.abspath('../pIMZ'))
 
 # -- Project information -----------------------------------------------------
 
@@ -29,8 +29,9 @@ master_doc = 'index'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.napoleon",
-    'sphinxcontrib.apidoc'
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.mathjax',
 ]
 
 
