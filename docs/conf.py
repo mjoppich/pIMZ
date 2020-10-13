f735dcbd0476a7a6de289951361fe6067b08710c
mjoppich <github@mjoppich.net>
2020-10-13 15:57:43 +0200

    readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ autodoc_mock_imports = [
         'seaborn',
         'llvmlite',
         'imageio', 'umap', 'jinja2', 'hdbscan', 'regex',
-        'Pillow']
+        'Pillow', 'mpl_toolkits']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
