89a4a8d0d31659351ecf6488e2bd3a7eee73b766
Markus <markus@weismann>
2022-11-28 18:55:32 +0100

    fix ci

--- pIMZ/enrichment.py
+++ pIMZ/enrichment.py
@@ -70,11 +70,106 @@ from statsmodels.stats.multitest import multipletests
 from scipy.stats import hypergeom
 
 
+class EnrichmentAnalysis(metaclass=abc.ABCMeta):
+    
+    def __init__(self):
+        pass
+    
+    def custom_div_cmap(self, numcolors=11, name='custom_div_cmap',
+                        mincol='blue', midcol='white', maxcol='red'):
+        """ Create a custom diverging colormap with three colors
+        
+        Default is blue to white to red with 11 colors.  Colors can be specified
+        in any way understandable by matplotlib.colors.ColorConverter.to_rgb()
+        """
+
+        from matplotlib.colors import LinearSegmentedColormap 
+        
+        cmap = LinearSegmentedColormap.from_list(name=name, 
+                                                colors = [x for x in [mincol, midcol, maxcol] if not x is None],
+                                                N=numcolors)
+        return cmap
+
+    def plotORAresult( self, dfin, title, numResults=10, figsize=(10,10)):
+        #https://www.programmersought.com/article/8628812000/
+        
+        def makeTitle(colDescr, colID, colSize, sucSize, setSize):
+            out = []
+            for x,y,z,s,p in zip(colDescr, colID, colSize, sucSize, setSize):
+                out.append("{} ({}, cov={:.3f}={}/{})".format(x, y, z, s, p))
+
+            return out
 
 
-class OverrepresentationAnalysis:
+        df_raw = dfin.copy()
+
+        # Prepare Data
+        #determine plot type
+
+        termIDColumn = "elem_id"
+        termNameColumn = "elem_id"
+        df = df_raw.copy()
+
+
+        rvb = None
+
+        color1 = "#883656"
+        color2 = "#4d6841"
+        color3 = "#afafaf"
+
+        if "sample_success_fraction" in df_raw.columns:
+            #ORA
+            rvb = self.custom_div_cmap(150, mincol=color2, maxcol=color3, midcol=None)
+            colorValues = [rvb(x/df.sample_success_fraction.max()) for x in df.sample_success_fraction]
+
+        else:
+            raise ValueError()
+
+        df['termtitle'] = makeTitle(df_raw[termNameColumn], df_raw[termIDColumn], df["sample_success_fraction"], df["success_samples"], df["sample_size"])
+
+
+        df.reset_index()
+        df = df[:numResults]
+        colorValues = colorValues[:numResults]
+        
+        df = df.iloc[::-1]
+        colorValues = colorValues[::-1]
+
+        print(df.shape)
+
+        if df.shape[0] == 0:
+            return
+        
+        maxNLog = max(-np.log(df.adj_pval))
+        maxLine = ((maxNLog// 10)+1)*10       
+        
+        # Draw plot
+        fig, ax = plt.subplots(figsize=figsize, dpi= 80)
+        ax.hlines(y=df.termtitle, xmin=0, xmax=maxLine, color='gray', alpha=0.7, linewidth=1, linestyles='dashdot')
+        ax.vlines(x=-np.log(0.05), ymin=0, ymax=numResults, color='red', alpha=0.7, linewidth=1, linestyles='dashdot')
+        
+        sizeFactor = 10    
+        scatter = ax.scatter(y=df.termtitle, x=-np.log(df.adj_pval), s=df.sample_size*sizeFactor, c=colorValues, alpha=0.7, )
+
+        handles, labels = scatter.legend_elements(prop="sizes", alpha=0.6, func=lambda x: x/sizeFactor)
+        labels = [x for x in labels]
+
+        # Title, Label, Ticks and Ylim
+        ax.set_title(title, fontdict={'size':12})
+        ax.set_xlabel('Neg. Log. Adj. p-Value')
+        ax.set_yticks(df.termtitle)
+        ax.set_yticklabels(df.termtitle, fontdict={'horizontalalignment': 'right'})
+        plt.grid(b=None)
+        plt.tight_layout()
+        plt.yticks(fontsize=16)
+        plt.show()
+
+
+
+class OverrepresentationAnalysis(EnrichmentAnalysis):
     
     def __init__(self, spec:SpectraRegion, ann:ProteinWeights):
+        
         
         self.spec = spec
         self.ann = ann
@@ -128,6 +223,7 @@ class OverrepresentationAnalysis:
             
             resultObj = {
                 'elem_id': pathwayID,
+                'elem_name': pathwayName,
                 'population_size': measuredGenes,
                 'success_population': len(significantFeatures),
                 'sample_size': sampleSize,
